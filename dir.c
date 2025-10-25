de41905952af1191c662f9a4ea1880934b97ef0b
Evers Chen <evers_chen@163.com>
2025-10-26 00:07:46 +0800

    ls hung issue

--- dir.c
+++ dir.c
@@ -6,8 +6,45 @@
 #include "dir.h"
 #include "super.h"
 
-static struct dentry *ecfs_mkdir(struct mnt_idmap *idmap, struct inode *dir,
-                                struct dentry *dentry, umode_t mode)
+static int ecfs_readdir(struct file *file, struct dir_context *ctx)
+{
+    struct inode *inode = file_inode(file);
+    struct ecfs_inode *ei = inode->i_private;
+
+    if (ctx->pos == 0) {
+        if (!dir_emit_dot(file, ctx))
+            return 0;
+        ctx->pos = 1;
+    }
+
+    if (ctx->pos == 1) {
+        ino_t parent_ino = ei->parent_ino ? ei->parent_ino : inode->i_ino;
+        if (!dir_emit(ctx, "..", 2, parent_ino, DT_DIR))
+            return 0;
+        ctx->pos = 2;
+    }
+
+    return 0;
+}
+
+static struct dentry *ecfs_lookup(struct inode *dir, struct dentry *dentry, unsigned int flags)
+{
+    struct inode *inode = NULL;
+
+    if (strcmp(dentry->d_name.name, "mydir") == 0) {
+        inode = ecfs_new_inode(dir->i_sb, dir, S_IFDIR | 0755);
+    } else if (strcmp(dentry->d_name.name, "file.txt") == 0) {
+        inode = ecfs_new_inode(dir->i_sb, dir, S_IFREG | 0644);
+    }
+
+    if (!inode)
+        return ERR_PTR(-ENOENT);
+
+    d_add(dentry, inode);
+    return NULL;
+}
+
+static struct dentry * ecfs_mkdir(struct mnt_idmap *idmap, struct inode *dir, struct dentry *dentry, umode_t mode)
 {
     struct inode *inode;
     struct dentry *ret;
@@ -47,14 +84,39 @@ static int ecfs_create(struct mnt_idmap *idmap, struct inode *dir,
     return 0;
 }
 
-const struct inode_operations ecfs_dir_inode_ops = {
-    .create = ecfs_create,   // 返回 int
-    .mkdir  = ecfs_mkdir,    // 返回 struct dentry *
-    .lookup = simple_lookup,
+static int ecfs_unlink(struct inode *dir, struct dentry *dentry)
+{
+    struct inode *inode = d_inode(dentry);
+    drop_nlink(inode);
+    d_delete(dentry);
+    dput(dentry);
+    return 0;
+}
+
+static int ecfs_rmdir(struct inode *dir, struct dentry *dentry)
+{
+    struct inode *inode = d_inode(dentry);
+    if (!S_ISDIR(inode->i_mode)) return -ENOTDIR;
+    if (inode->i_nlink > 2) return -ENOTEMPTY;
+    drop_nlink(inode);
+    drop_nlink(dir);
+    d_delete(dentry);
+    dput(dentry);
+    return 0;
+}
+
+// 关键：使用 iterate_shared
+const struct file_operations ecfs_dir_ops = {
+    .llseek         = generic_file_llseek,
+    .read           = generic_read_dir,
+    .iterate_shared = ecfs_readdir,
+    .fsync          = noop_fsync,
 };
 
-const struct file_operations ecfs_dir_ops = {
-    .open           = simple_open,
-    .iterate_shared = iterate_dir,
-    .llseek         = generic_file_llseek,
+const struct inode_operations ecfs_dir_inode_ops = {
+    .lookup         = ecfs_lookup,
+    .mkdir          = ecfs_mkdir,
+    .rmdir          = ecfs_rmdir,
+    .create         = ecfs_create,
+    .unlink         = ecfs_unlink,
 };
